119fa1b695d337f847edc643c55b7743e066e029
verypay <136692996+verypay@users.noreply.github.com>
2023-06-16 11:34:47 +0700

    perf: add `redis` plugin modules
    packages:
    - @medusajs/cache-redis
    - @medusajs/event-bus-redis

--- medusa-config.js
+++ medusa-config.js
@@ -53,7 +53,7 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL
@@ -64,7 +64,7 @@ const modules = {
     options: {
       redisUrl: REDIS_URL
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
@@ -75,7 +75,7 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL
+  redis_url: REDIS_URL
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
